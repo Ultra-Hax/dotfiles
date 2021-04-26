5d999c033f5eacddeb195f939b7cdb00d1e7e300
Ultra-Hax <68920205+Ultra-Hax@users.noreply.github.com>
2021-04-26 18:41:58 -0400

    fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+
+fundle plugin 'danhper/fish-theme-afowler'
+fundle plugin 'pure-fish/pure' --url 'https://github.com/pure-fish/pure.git'
+fundle init
