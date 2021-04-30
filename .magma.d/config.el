d8224d8d6de38f18692c29e0388db76ca213c153
Ultra-Hax <68920205+Ultra-Hax@users.noreply.github.com>
2021-04-30 16:14:08 -0400

    Add .magma.d

---
+++ .magma.d/config.el
@@ -0,0 +1,6 @@
+;;; config.el user configuration for magmamacs -*- lexical-binding: t -*-
+
+
+(load-theme 'doom-moonlight t)
+(add-to-list 'default-frame-alist '(font . "Monoid Nerd Font Mono-10" ))
+(set-face-attribute 'default t :font "Monoid Nerd Font Mono-10" )
